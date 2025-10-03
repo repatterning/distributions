262f8aafeacc1fc9b8c466665aac297417372251
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-03 16:20:47 +0100

    Add src/assets/interface.py

---
+++ src/assets/interface.py
@@ -0,0 +1,55 @@
+"""Module interface.py"""
+import typing
+import sys
+
+import pandas as pd
+
+import src.assets.foci
+import src.assets.gauges
+import src.assets.partitions
+import src.elements.s3_parameters as s3p
+import src.elements.service as sr
+import src.functions.cache
+
+
+class Interface:
+    """
+    Notes<br>
+    ------<br>
+
+    Reads-in the assets.
+    """
+
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters, arguments: dict):
+        """
+
+        :param service: A suite of services for interacting with Amazon Web Services.
+        :param s3_parameters: The overarching S3 parameters settings of this
+                              project, e.g., region code name, buckets, etc.
+        :param arguments: A set of arguments vis-à-vis calculation & storage objectives.
+        """
+
+        self.__service = service
+        self.__s3_parameters = s3_parameters
+        self.__arguments = arguments
+
+    def exc(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+
+        # Warning data
+        foci = src.assets.foci.Foci(s3_parameters=self.__s3_parameters).exc()
+
+        # Applicable time series metadata, i.e., gauge, identification codes
+        gauges = src.assets.gauges.Gauges(
+            service=self.__service, s3_parameters=self.__s3_parameters, arguments=self.__arguments).exc()
+        if gauges.empty:
+            src.functions.cache.Cache().exc()
+            sys.exit('There are no data sets for model development.')
+
+        partitions, _ = src.assets.partitions.Partitions(
+            gauges=gauges, foci=foci, arguments=self.__arguments).exc()
+
+        return partitions
