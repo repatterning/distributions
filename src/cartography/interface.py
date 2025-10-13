a8c5ecdee5d61d1067070c549a89c022e1d1d14d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-13 20:33:51 +0100

    Update src/cartography/interface.py

--- src/cartography/interface.py
+++ src/cartography/interface.py
@@ -1,18 +1,16 @@
 """Module cartography/interface.py"""
-import pathlib
 
 import boto3
-import pandas as pd
 import geopandas
+import pandas as pd
 
 import src.cartography.data
+import src.cartography.illustrate
 import src.cartography.maps
+import src.cartography.reference
 import src.cartography.risks
 import src.elements.s3_parameters as s3p
-import src.elements.service as sr
 import src.s3.keys
-import src.cartography.illustrate
-import src.cartography.reference
 
 
 class Interface:
@@ -20,17 +18,15 @@ class Interface:
     An interface to the risks programs
     """
 
-    def __init__(self, connector: boto3.session.Session, service: sr.Service, s3_parameters: s3p.S3Parameters):
+    def __init__(self, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters):
         """
 
         :param connector: An instance of boto3.session.Session
-        :param service: A suite of services for interacting with Amazon Web Services.
         :param s3_parameters: The overarching S3 parameters settings of this
                               project, e.g., region code name, buckets, etc.<br>
         """
 
         self.__connector = connector
-        self.__service = service
         self.__s3_parameters = s3_parameters
 
         # Instances
